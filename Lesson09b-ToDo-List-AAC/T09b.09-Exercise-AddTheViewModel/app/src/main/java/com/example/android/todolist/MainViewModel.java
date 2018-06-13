4c987455258de462ce706d777ad67a4989b9060c
Eric Njogu <kunadawa@gmail.com>
2018-06-13 08:55:51 +0300

    wrapping the data retrieval for all tasks in a ViewModel

--- Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
+++ Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -1,11 +1,35 @@
 package com.example.android.todolist;
 
-// TODO (1) make this class extend AndroidViewModel and implement its default constructor
-public class MainViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
-    // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
 
-    // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+import java.util.List;
 
-    // TODO (3) Create a getter for the tasks variable
+// DO (1) make this class extend AndroidViewModel and implement its default constructor
+public class MainViewModel extends AndroidViewModel {
+    private LiveData<List<TaskEntry>> tasks;
+    // Constant for logging
+    private static final String TAG = MainViewModel.class.getSimpleName();
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        Log.d(TAG, "Retrieving all tasks from dataBase");
+        tasks = AppDatabase.getInstance(getApplication().getApplicationContext()).taskDao().loadAllTasks();
+    }
+
+    public LiveData<List<TaskEntry>> getTasks() {
+        return tasks;
+    }
+
+    // DO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+
+    // DO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
+
+    // DO (3) Create a getter for the tasks variable
 }
