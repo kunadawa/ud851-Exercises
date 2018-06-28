b9b3e66ef95a719b740d2536e96d1d049151890f
Eric Njogu <kunadawa@gmail.com>
2018-06-28 10:02:26 +0300

    splitting the layout file into includes and customizing the landscape file
    The landscape customization did not really work since the designer was very unstable and would not respond to changes in the text view

--- Lesson11-Completeing-The-UI/T11.03-Exercise-LandscapeLayout/app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ Lesson11-Completeing-The-UI/T11.03-Exercise-LandscapeLayout/app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -51,18 +51,18 @@ public class MainActivity extends AppCompatActivity {
 
         mBinding.textViewPassengerName.setText(info.passengerName);
         // TODO (7) Use the flightInfor attribute in mBinding below to get the appropiate text Views
-        mBinding.textViewOriginAirport.setText(info.originCode);
-        mBinding.textViewFlightCode.setText(info.flightCode);
-        mBinding.textViewDestinationAirport.setText(info.destCode);
+        mBinding.flightInfo.textViewOriginAirport.setText(info.originCode);
+        mBinding.flightInfo.textViewFlightCode.setText(info.flightCode);
+        mBinding.flightInfo.textViewDestinationAirport.setText(info.destCode);
 
         SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault());
         String boardingTime = formatter.format(info.boardingTime);
         String departureTime = formatter.format(info.departureTime);
         String arrivalTime = formatter.format(info.arrivalTime);
 
-        mBinding.textViewBoardingTime.setText(boardingTime);
-        mBinding.textViewDepartureTime.setText(departureTime);
-        mBinding.textViewArrivalTime.setText(arrivalTime);
+        mBinding.flightInfo.textViewBoardingTime.setText(boardingTime);
+        mBinding.flightInfo.textViewDepartureTime.setText(departureTime);
+        mBinding.flightInfo.textViewArrivalTime.setText(arrivalTime);
 
         long totalMinutesUntilBoarding = info.getMinutesUntilBoarding();
         long hoursUntilBoarding = TimeUnit.MINUTES.toHours(totalMinutesUntilBoarding);
@@ -73,11 +73,11 @@ public class MainActivity extends AppCompatActivity {
                 hoursUntilBoarding,
                 minutesLessHoursUntilBoarding);
 
-        mBinding.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
+        mBinding.flightInfo.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
         // TODO (8) Use the boardingInfo attribute in mBinding below to get the appropiate text Views
-        mBinding.textViewTerminal.setText(info.departureTerminal);
-        mBinding.textViewGate.setText(info.departureGate);
-        mBinding.textViewSeat.setText(info.seatNumber);
+        mBinding.boardingInfo.textViewTerminal.setText(info.departureTerminal);
+        mBinding.boardingInfo.textViewGate.setText(info.departureGate);
+        mBinding.boardingInfo.textViewSeat.setText(info.seatNumber);
     }
 }
 
